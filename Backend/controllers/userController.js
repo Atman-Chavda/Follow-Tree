271f202c47a84c6feed45052c6dd9947ca138611
AtmanChavda <atmanchavda@gmail.com>
2024-09-14 18:09:19 +0530

    login created

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -26,12 +26,10 @@ export const signup = async (req, res) => {
     );
 
     const token = createSecretToken(email);
-    res.cookie('token',token,
-        {
-            withCredentials: true,
-            httpOnly: true, 
-        }
-    )
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: true,
+    });
 
     return res.status(201).json({
       success: true,
@@ -42,6 +40,32 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+    try
+    {
+        const {email, password} = req.body;
+        const checkUser = await conn.query(`SELECT * FROM user_details WHERE email = ?`, [email]);
+        if(checkUser[0].length === 0)
+        {
+            return res.status(400).json({success: false, message: "User not found"});
+        }
+        const validPassword = await bcrypt.compare(password, checkUser[0][0].password);
+        if(!validPassword)
+        {
+            return res.status(400).json({success: false, message: "Invalid password"});
+        }
+        const token = createSecretToken(email);
+        res.cookie("token", token, {
+            withCredentials: true,
+            httpOnly: true
+        });
+        res.status(200).json({success: true, message: "Login successful"});
+    }
+    catch(error)
+    {
+        console.log(error);
+        res.status(500).json({message: error.message});
+    }   
+};
 export const reset = async (req, res) => {};
 export const logout = async (req, res) => {};
